abaf2bef1a768c3ae79af14946e6aa3cda8d08c6
Kshitij Lauria <kslauria@gmail.com>
2016-02-25 22:06:33 -0500

    Split into a timing test-bed and individual implementations (more forthcoming)

--- cpu-simple.cxx
+++ cpu-simple.cxx
@@ -1,16 +1,7 @@
-#include <stdlib.h> // malloc, srand, rand
 #include <stddef.h> // size_t
-#include <string.h> // stoi
-
-#include <assert.h>
-
-#include <iostream> // std::cout
-#include <chrono> // std::chrono::high_resolution_clock
-
-#include "print-matrix.hxx"
 
 template<typename TT>
-void matrix_multiply_simple(const TT * AA, const TT * BB, TT * CC, size_t nn) {
+void matrix_multiply(const TT * AA, const TT * BB, TT * CC, size_t nn) {
     for (size_t ii = 0; ii < nn; ++ii) {
         for (size_t jj = 0; jj < nn; ++jj) {
             size_t index = ii*nn + jj;
@@ -22,46 +13,5 @@ void matrix_multiply_simple(const TT * AA, const TT * BB, TT * CC, size_t nn) {
     }
 }
 
-void timetrial(size_t nn, unsigned long ntrials) {
-    
-    typedef double real_t;
-    real_t *aa, *bb, *cc;
-
-    aa = (real_t *)malloc(sizeof(real_t)*nn*nn);
-    bb = (real_t *)malloc(sizeof(real_t)*nn*nn);
-    cc = (real_t *)malloc(sizeof(real_t)*nn*nn);
-
-    srand(1);
-    for(size_t ii = 0; ii < nn*nn; ++ii) {
-        aa[ii] = (real_t)rand() / RAND_MAX;
-        bb[ii] = (real_t)rand() / RAND_MAX;
-    }
-
-    auto begin = std::chrono::high_resolution_clock::now();
-
-    for(unsigned ii = 0; ii < ntrials; ++ii)
-        matrix_multiply_simple(aa, bb, cc, nn);
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    // 1e-6 seconds
-    double micro_seconds = std::chrono::duration<double, std::micro>(end-begin).count();
-
-    // ntrials * nn^3 mults, same number of adds
-    double megaflops = 2*nn*nn*nn*ntrials/micro_seconds;
-
-    
-    std::cout << "N = " << nn << ", megaflops = " <<  megaflops << ", size = " << 3*nn*nn*sizeof(real_t) << ", ntrials = " << ntrials << "\n";
-
-    free(aa); free(bb); free(cc);
-}
-
-int main(int argc, char **argv) {
-    for(int ii = 5; ii < 2000; ++ii) {
-        size_t nn = ii;
-        timetrial(nn, std::max(1e9/(nn*nn*nn), 5.0));
-        std::cout.flush();
-    }    
-        
-    return 0;
-}
+template void matrix_multiply<float>(const float*, const float*, float*, size_t);
+template void matrix_multiply<double>(const double*, const double*, double*, size_t);
