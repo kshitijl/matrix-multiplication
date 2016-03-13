f2bc5ec45abb904d3047f7130342b9f237dba926
Kshitij Lauria <kslauria@gmail.com>
2016-03-13 00:14:02 -0500

    First correct gpu tiled version: can deal with any dimension

--- gpu-tiled.cu
+++ gpu-tiled.cu
@@ -1,33 +1,48 @@
 #include "cuda-common.hxx"
 
-const int TILESIZE = 16;
+const int TILESIZE = 32;
 
 template<typename TT>
 __global__ void mm_kernel(const TT * AA, const TT * BB, TT * CC, size_t nn) {
     __shared__ TT sA[TILESIZE][TILESIZE];
     __shared__ TT sB[TILESIZE][TILESIZE];
 
+    unsigned int tx = threadIdx.x, ty = threadIdx.y;
+
     // each thread computes 1 element of the answer
-    unsigned int row_C = blockIdx.y * TILESIZE + threadIdx.y;
-    unsigned int col_C = blockIdx.x * TILESIZE + threadIdx.x;
+    unsigned int row_C = blockIdx.y * TILESIZE + ty;
+    unsigned int col_C = blockIdx.x * TILESIZE + tx;
 
     // loop over all the tiles necessary to compute the answer
     TT answer = 0;
-    for(int tile = 0; tile < nn/TILESIZE; ++tile) {
+    for(int tile = 0; tile < (nn + TILESIZE - 1)/TILESIZE; ++tile) {
         // all threads cooperatively load a tile of A and a tile of B
         // into shared memory
-        sA[threadIdx.y][threadIdx.x] = AA[row_C*nn + tile*TILESIZE + threadIdx.x];
-        sB[threadIdx.y][threadIdx.x] = BB[(tile*TILESIZE + threadIdx.y)*nn + col_C];
+        if(row_C < nn and tile*TILESIZE + tx < nn) {
+            // grab AA[row_C][tile*TILESIZE + tx]
+            sA[ty][tx] = AA[row_C*nn + tile*TILESIZE + tx];
+        } else {
+            sA[ty][tx] = 0;
+        }
+        if(col_C < nn and tile*TILESIZE + ty < nn) {
+            // grab BB[tile*TILESIZE + ty][col_C]
+            sB[ty][tx] = BB[(tile*TILESIZE + ty)*nn + col_C];
+        }
+        else {
+            sB[ty][tx] = 0;
+        }
+
         __syncthreads();
 
         // now add up the partial answer from this tile. Each thread
         // only uses a single row of AA and a single column of BB
         for(int kk = 0; kk < TILESIZE; ++kk) {
-            answer += sA[threadIdx.y][kk] * sB[kk][threadIdx.x];
+            answer += sA[ty][kk] * sB[kk][tx];
         }
         __syncthreads();
     }
-    CC[row_C*nn + col_C] = answer;
+    if(row_C < nn and col_C < nn)
+        CC[row_C*nn + col_C] = answer;
 }
 
 template<typename TT>
